d0ddc95b818e15b709946b69a6c4d986a99c3697
Jan Horak <janhorak58@gmail.com>
2022-08-03 08:31:49 +0200

    google signin with redirect (will be deleted)

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 import {
+  auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+  useEffect(() => {
+    const loginHandler = async () => {
+      const response = await getRedirectResult(auth);
+      if (response) {
+        const { user } = response;
+        const userDocRef = await createUserDocumentFromAuth(user);
+      }
+    };
+    loginHandler();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
+  };
 
+  const logGoogleRedirectUser = async () => {
+    const { user } = await signInWithGoogleRedirect();
+    // const userDocRef = await createUserDocumentFromAuth(user);
+    console.log(user);
   };
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign In With Google Popup</button>
+      <button onClick={logGoogleRedirectUser}>
+        Sign In With Google Redirect
+      </button>
     </div>
   );
 };
